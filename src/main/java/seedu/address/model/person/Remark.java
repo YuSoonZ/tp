01189ccea1c12e63fa559e654052824fe35c0e30
HENRY\YS <yusoonng3@gmail.com>
2024-02-23 17:59:06 +0800

    Update RemarkCommand and RemarkCommandParser

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,10 +9,17 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Constructs a new Remark object with the specified remark value.
+     *
+     * @param remark the remark value to be associated with this Remark object
+     * @throws NullPointerException if the specified remark is null
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
     }
+
 
     @Override
     public String toString() {
